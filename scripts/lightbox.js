be6c2dbc97b743be3804e3d3aa06f25aca19fec8
Lucas Lago <lcslago97@gmail.com>
2023-04-01 13:20:19 -0300

    Adequação de código no script.js

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,5 +1,19 @@
+function controlSlide () {
+    let prev = document.querySelector(".product-page__main-gallery--prev");
+    let next = document.querySelector(".product-page__main-gallery--next");
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    })
+
+    next.addEventListener("click", () => {
+        plusSlides(1);
+    })
+}
+
 function showSlides(n) {
     let slides = document.getElementsByClassName("main-modal__content-slide");
+
     if (n > slides.length) {
         slideIndex = 1
     }
@@ -18,3 +32,4 @@ function plusSlides(n) {
 
 let slideIndex = 1;
 showSlides(slideIndex);
+controlSlide();
