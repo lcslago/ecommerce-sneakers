ff237f55c515f6c801a6882dc2c8c38445f998e7
Lucas Lago <lcslago97@gmail.com>
2023-04-05 20:48:44 -0300

    Melhorias no carrinho, localStorage implementado

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,11 +1,20 @@
-let cartNumber = controlCartCount();
+let minus = document.querySelector(".cart-counter--minus");
+
+let plus = document.querySelector(".cart-counter--plus");
+
+let number = parseInt(document.querySelector(".cart-counter__number").innerHTML);
+
+let button = document.querySelector(".button");
+
+let counterNotification = document.querySelector(".header__cart-count");
+
+let cartNumber = buttonCart();
+
+let newCartNumber;
+
+saveCartNumber();
 
 function controlCartCount() {
-    let minus = document.querySelector(".cart-counter--minus");
-    let plus = document.querySelector(".cart-counter--plus");
-    let number = parseInt(document.querySelector(".cart-counter__number").innerHTML);
-    let button = document.querySelector(".button");
-    let counterNotification = document.querySelector(".header__cart-count");
 
     minus.addEventListener("click", () => {
         if (number <= 0) {
@@ -20,6 +29,10 @@ function controlCartCount() {
         number += 1;
         cartNumber = document.querySelector(".cart-counter__number").innerHTML = number;
     });
+}
+
+function buttonCart() {
+    controlCartCount();
 
     button.addEventListener("click", () => {
         if (number < 1) {
@@ -27,12 +40,32 @@ function controlCartCount() {
         } else {
             counterNotification.style.display = "block";
             document.querySelector(".header__cart-count").innerHTML = cartNumber;
+            newCartNumber = parseInt(localStorage.setItem("cart", cartNumber));
         }
     })
 }
 
+function showCartNotification() {
+    counterNotification.style.display = "block";
 
+    document.querySelector(".header__cart-count").innerHTML = localStorage.getItem("cart", cartNumber);
 
+    number = document.querySelector(".cart-counter__number").innerHTML = parseInt(localStorage.getItem("cart", cartNumber));
 
+    if (number == parseInt(localStorage.getItem("cart", cartNumber))) {
+        button.addEventListener("click", () => {
+            localStorage.setItem("cart", number);
+            document.querySelector(".header__cart-count").innerHTML = number;
+        })
+    }
+}
 
-
+function saveCartNumber() {
+    window.onload = () => {
+        if (localStorage.getItem("cart", cartNumber) < 1) {
+            counterNotification.style.display = "none";
+        } else {
+            showCartNotification();
+        }
+    }
+}
