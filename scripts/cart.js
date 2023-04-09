33ecc52956d0a1aef8e67b6abf15198e325442aa
Lucas Lago <lcslago97@gmail.com>
2023-04-08 22:10:45 -0300

    Adicionada a opção de apagar item do carrinho através de um menu dropdown

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,3 +1,9 @@
+const productName = document.querySelector(".product-name").innerHTML;
+
+const productPrice = document.querySelector(".product-price").innerHTML;
+
+// const productValue = parseInt(document.querySelector(".product-price__value").innerHTML);
+
 let minus = document.querySelector(".cart-counter--minus");
 
 let plus = document.querySelector(".cart-counter--plus");
@@ -8,11 +14,27 @@ let button = document.querySelector(".button");
 
 let counterNotification = document.querySelector(".header__cart-count");
 
+const cartButton = document.querySelector("#cart");
+
+const cartMenu = document.querySelector(".header__cart-items");
+
+const cartProductlist = document.querySelector(".header__cart-product");
+
+const trashBin = document.querySelector(".header__cart-product--delete");
+
+const cartOverview = document.querySelector(".cart__overview");
+
+let totalSum = document.querySelector(".cart__total--sum").innerHTML;
+
 let cartNumber = buttonCart();
 
 let newCartNumber;
 
 saveCartNumber();
+
+showCartMenu();
+
+deleteCart();
 
 function controlCartCount() {
 
@@ -37,10 +59,13 @@ function buttonCart() {
     button.addEventListener("click", () => {
         if (number < 1) {
             counterNotification.style.display = "none";
+            cartProductlist.style.display = "none";
+            cartOverview.style.display = "none"
         } else {
             counterNotification.style.display = "block";
             document.querySelector(".header__cart-count").innerHTML = cartNumber;
             newCartNumber = parseInt(localStorage.setItem("cart", cartNumber));
+            showCartItem();
         }
     })
 }
@@ -67,5 +92,38 @@ function saveCartNumber() {
         } else {
             showCartNotification();
         }
+        showCartItem();
     }
+}
+
+function showCartMenu() {
+    cartButton.addEventListener("click", () => {
+        cartMenu.classList.toggle("show");
+    });
+}
+
+function showCartItem() {
+
+    document.querySelector(".item__name").innerHTML = productName;
+    document.querySelector(".item__price").innerHTML = `$${productPrice}`;
+    document.querySelector(".item__quantity").innerHTML = number;
+
+    if (number > 0) {
+        cartProductlist.style.display = "flex";
+        cartOverview.style.display = "block";
+        totalSum = document.querySelector(".cart__total--sum").innerHTML = `$${parseFloat(productPrice * number).toFixed(2)}`;
+        console.log(totalSum);
+    } else {
+        cartProductlist.style.display = "none";
+        cartOverview.style.display = "none";
+    }
+}
+
+function deleteCart() {
+    trashBin.addEventListener("click", () => {
+        localStorage.clear("cart");
+        cartProductlist.style.display = "none";
+        counterNotification.style.display = "none";
+        cartOverview.style.display = "none";
+    })
 }
