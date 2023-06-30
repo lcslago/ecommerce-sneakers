d43dd5a0629c84eda09ed230c6cce9c4d83bad94
Lucas Lago <lcslago97@gmail.com>
2023-06-29 22:23:58 -0300

    a galeria pro layout desktop foi finalizada

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,61 +1,101 @@
-function controlSlide() {
+(function () {
     let prev = document.querySelector(".product-page__main-gallery--prev");
     let next = document.querySelector(".product-page__main-gallery--next");
 
     prev.addEventListener("click", () => {
         plusSlides(-1);
+        thumbnailModal[slideIndex].classList.toggle("active");
+        thumbnailModal[slideIndex + 1].classList.remove("active");
     })
 
     next.addEventListener("click", () => {
         plusSlides(1);
+        thumbnailModal[slideIndex].classList.toggle("active");
+        thumbnailModal[slideIndex - 1].classList.remove("active");
     })
-}
+})();
+
+const thumbnailModal = document.querySelectorAll('[data-thumbmodal]');
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("main-modal__content-slide");
 
-    if (n > slides.length) {
-        slideIndex = 1
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+        thumbnailModal[n - 1].classList.remove("active");
     }
-    if (n < 1) {
-        slideIndex = slides.length
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+        thumbnailModal[n + 1].classList.remove("active");
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex].style.display = "block";
+
+    for (let i = 0; i < thumbnailModal.length; i++) {
+        thumbnailModal[i].addEventListener('click', () => {
+            slideIndex = i;
+            showSlides(slideIndex);
+            for (let j = 0; j < thumbnailModal.length; j++) {
+                thumbnailModal[j].classList.remove("active");
+            }
+            thumbnailModal[slideIndex].classList.toggle("active");
+        })
+
+    }
+
+    relateModalAndMain();
 }
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    slideIndex += n
+    showSlides(slideIndex);
+
 }
 
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
-controlSlide();
 
 const galleryModal = document.querySelector('[data-modal]');
 
-const openGallery = document.querySelectorAll('[data-enter]');
-const lightboxOpened = document.querySelectorAll('[data-lightbox]');
-for (let i = 0; i < openGallery.length; i++) {
-    openGallery[i].addEventListener('click', () => {
-        galleryModal.style.display = "flex";
-        for (let j = 0; j < lightboxOpened.length; j++) {
-            lightboxOpened[j].style.display = "none";
-        }
-        lightboxOpened[i].style.display = "flex";
-    })
-};
+function relateModalAndMain() {
+    const openGallery = document.querySelectorAll('[data-enter]');
+    const lightboxOpened = document.querySelectorAll('[data-lightbox]');
+
+    for (let i = 0; i < openGallery.length; i++) {
+        openGallery[i].addEventListener('click', () => {
+            galleryModal.style.display = "flex";
+            slideIndex = i;
+            showSlides(slideIndex);
+            thumbnailModal[i].classList.add("active");
+
+            for (let j = 0; j < lightboxOpened.length; j++) {
+                lightboxOpened[j].style.display = "none";
+
+            }
+            lightboxOpened[i].style.display = "flex";
+
+        })
+    };
+}
 
 const closeButton = document.querySelector('[data-exit]');
-closeButton.addEventListener('click', () => { galleryModal.style.display = "none" })
+closeButton.addEventListener('click', () => {
+    galleryModal.style.display = "none";
+    for (let i = 0; i < thumbnailModal.length; i++) {
+        thumbnailModal[i].classList.remove("active");
+
+    }
+
+})
 closeButton.addEventListener('contextmenu', (e) => { e.preventDefault() })
 
 const mainGalleryThumbnails = document.querySelectorAll('[data-thumb]');
 const mainGallerySlides = document.querySelectorAll(".main-gallery__container");
 
-function thumbControl() {
+(function () {
     for (let i = 0; i < mainGallerySlides.length; i++) {
         mainGallerySlides[i].style.display = "none";
     }
@@ -72,6 +112,4 @@ function thumbControl() {
             mainGalleryThumbnails[i].classList.toggle("active");
         })
     }
-}
-
-thumbControl();
+})();
